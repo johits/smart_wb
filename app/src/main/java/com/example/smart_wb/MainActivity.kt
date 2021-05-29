d0956a6ce486eabbd928feeb9696bfd90e5fc4da
johits <74091087+johits@users.noreply.github.com>
2021-05-29 16:10:19 +0900

    충돌충돌충돌

--- app/src/main/java/com/example/smart_wb/MainActivity.kt
+++ app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //2020-05-29
-//abc
+//충돌테스트트트트트트
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        //오늘프래그먼트까지가능할것인가
     }
 }
