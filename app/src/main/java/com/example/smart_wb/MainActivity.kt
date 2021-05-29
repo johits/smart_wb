456a25ae2983332c8a7711fff9319e8967f7dffe
johits <74091087+johits@users.noreply.github.com>
2021-05-29 15:34:32 +0900

    브런치 연동 제대로 되었나 확인

--- app/src/main/java/com/example/smart_wb/MainActivity.kt
+++ app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.smart_wb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//2020-05-29
+//2020-05-29 푸쉬
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
