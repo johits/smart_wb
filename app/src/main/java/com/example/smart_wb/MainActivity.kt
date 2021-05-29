50172d4531da3236dd8d775de005e51c992f6bd5
johits <74091087+johits@users.noreply.github.com>
2021-05-29 19:12:52 +0900

    프래그먼트 세팅완료

--- app/src/main/java/com/example/smart_wb/MainActivity.kt
+++ app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.smart_wb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
-//2020-05-29
-//아무말대잔치
+//2020-05-29 joker 메인 클래스 (프래그먼트 메뉴에 대한 코드)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,19 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        //오늘프래그먼트까지가능할것인가
+        configureBottomNavigation()
+
     }
-}
+        private fun configureBottomNavigation(){
+            xml_main_viewpaper.adapter = AdapterMainFragment(supportFragmentManager, 5)
+            xml_main_tablayout.setupWithViewPager(xml_main_viewpaper)
+
+            val viewBtmNaviMain : View = this.layoutInflater.inflate(R.layout.bottom_navigation, null, false)
+
+            xml_main_tablayout.getTabAt(0)!!.customView = viewBtmNaviMain.findViewById(R.id.xml_btmnv_btn_profile)  as RelativeLayout
+            xml_main_tablayout.getTabAt(1)!!.customView = viewBtmNaviMain.findViewById(R.id.xml_btmnv_btn_friends)  as RelativeLayout
+            xml_main_tablayout.getTabAt(2)!!.customView = viewBtmNaviMain.findViewById(R.id.xml_btmnv_btn_search)   as RelativeLayout
+            xml_main_tablayout.getTabAt(3)!!.customView = viewBtmNaviMain.findViewById(R.id.xml_btmnv_btn_chat)     as RelativeLayout
+            xml_main_tablayout.getTabAt(4)!!.customView = viewBtmNaviMain.findViewById(R.id.xml_btmnv_btn_setting)  as RelativeLayout
+        }
+    }
