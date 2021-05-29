7f7373252be358fb82354b3c1bc55d4781f373e7
yamamamo <84963245+yamamamo@users.noreply.github.com>
2021-05-29 14:11:21 +0900

    주석

--- smart_wb/app/src/main/java/com/example/smart_wb/MainActivity.kt
+++ smart_wb/app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.smart_wb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/**
+ * copyright 2021/05/29
+ * */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
