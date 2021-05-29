7214ef44034d4525aab25b5bbd4eecd728a15cc6
johits <74091087+johits@users.noreply.github.com>
2021-05-29 14:37:18 +0900

    프로젝트 PUSH

---
+++ app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smart_wb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//2020-05-29
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
