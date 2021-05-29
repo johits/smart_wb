3258d0122851c78e67e8e91eb3f7653e94c6bb5e
yamamamo <84963245+yamamamo@users.noreply.github.com>
2021-05-29 15:59:41 +0900

    야마
    abc

--- app/src/main/java/com/example/smart_wb/MainActivity.kt
+++ app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //2020-05-29
-//가나다
+//abc
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
