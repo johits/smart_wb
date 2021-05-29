8dc41ddd863e0ca4952aff4524f3e3897a9e6f34
yamamamo <84963245+yamamamo@users.noreply.github.com>
2021-05-29 20:44:07 +0900

    MainActivity 주석

--- app/src/main/java/com/example/smart_wb/MainActivity.kt
+++ app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.smart_wb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//2020-05-29
-//아무말대잔치
+/**
+ * 2020-05-29 yama
+ * */
 
 
 class MainActivity : AppCompatActivity() {
