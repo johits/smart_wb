a3d88b05320d5520327db00bea63dd9d9db05403
yamamamo <84963245+yamamamo@users.noreply.github.com>
2021-05-30 09:58:42 +0900

    BottomNavi fragment 전환

--- app/src/main/java/com/example/smart_wb/MainActivity.kt
+++ app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.smart_wb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /**
- * 2020-05-29 yama
+ * 2020-05-29 yama 메인액티비티
  * */
 
 
@@ -12,5 +15,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //초기 바텀네비게이션 세팅
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, ScreenTimeFragment())
+            .commit()
+
+        transitonNavigationBottomView(findViewById(R.id.bottomnavigation), supportFragmentManager)
+    }
+
+
+    //프레그먼트 전환
+    fun transitonNavigationBottomView(
+        bottomView: BottomNavigationView,
+        fragmentManager: FragmentManager
+    ) {
+        bottomView.setOnNavigationItemSelectedListener {
+            it.isChecked = true
+            when (it.itemId) {
+                R.id.item_calender -> {
+                    fragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, CalenderFragment()).commit()
+                }
+                R.id.item_chart -> {
+                    fragmentManager.beginTransaction().replace(R.id.frame_layout, ChartFragment())
+                        .commit()
+                }
+                R.id.item_screen_time -> {
+                    fragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, ScreenTimeFragment()).commit()
+                }
+                R.id.item_item -> {
+                    fragmentManager.beginTransaction().replace(R.id.frame_layout, ItemFragment())
+                        .commit()
+                }
+                R.id.item_setting -> {
+                    fragmentManager.beginTransaction().replace(R.id.frame_layout, SettingFragment())
+                        .commit()
+                }
+                else -> Log.d("test", "error") == 0
+            }
+            Log.d("test", "final") == 0
+        }
     }
 }
