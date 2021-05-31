611ac845b5bb47f01bd15e9a4d95b18c9c2d426b
johits <74091087+johits@users.noreply.github.com>
2021-05-31 14:39:16 +0900

    아이템리사이클러뷰의 아이템 간격 줄 수 있는 매서드 넣은 클래스 삭제하고 FragmentItem.kt에 구분선 메서드 넣음

--- app/src/main/java/com/example/smart_wb/FragmentItem.kt
+++ app/src/main/java/com/example/smart_wb/FragmentItem.kt
@@ -5,19 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_item.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentItem.newInstance] factory method to
- * create an instance of this fragment.
- */
 
 /**
 2021-05-31
@@ -60,14 +57,18 @@ class FragmentItem : Fragment() {
     }
 
     private fun initRecycler() {
+
         val layoutManager = LinearLayoutManager(requireContext())
         irv.setLayoutManager(layoutManager)
         irv.layoutManager =
             LinearLayoutManager(requireContext()).also { it.orientation = LinearLayoutManager.HORIZONTAL }
 
+        //구분선 넣기 (Horizontal 인 경우 0, vertical인 경우 1 설정)
+        irv.addItemDecoration(DividerItemDecoration(requireContext(), 0))
 
         itemAdapter = ItemAdapter(requireContext())
         irv.adapter = itemAdapter
+
 
         datas.apply {
 
