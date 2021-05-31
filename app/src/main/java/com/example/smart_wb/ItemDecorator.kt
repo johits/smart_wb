acc9c241917c8de74d6b1e2d0b1b84662f35c120
johits <74091087+johits@users.noreply.github.com>
2021-05-31 14:23:43 +0900

    아이템리사이클러뷰의 아이템 간격 줄 수 있는 매서드 넣은 클래스

---
+++ app/src/main/java/com/example/smart_wb/ItemDecorator.kt
@@ -0,0 +1,30 @@
+package com.example.smart_wb
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+2021-05-31
+joker
+아이템 리사이클러뷰 간격 주기*/
+
+class VerticalItemDecorator(private val divHeight : Int) : RecyclerView.ItemDecoration() {
+
+    @Override
+    override fun getItemOffsets(outRect: Rect, view: View, parent : RecyclerView, state : RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.top = divHeight
+        outRect.bottom = divHeight
+    }
+}
+
+class HorizontalItemDecorator(private val divHeight : Int) : RecyclerView.ItemDecoration() {
+
+    @Override
+    override fun getItemOffsets(outRect: Rect, view: View, parent : RecyclerView, state : RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.left = divHeight
+        outRect.right = divHeight
+    }
+}
