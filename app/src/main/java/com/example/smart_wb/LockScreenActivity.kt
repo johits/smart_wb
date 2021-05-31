1a98af21b53c5f3dccab0a12d1a91d554e96dd4e
johits <74091087+johits@users.noreply.github.com>
2021-05-31 18:50:17 +0900

    2021-05-31 퇴근전 커밋

--- app/src/main/java/com/example/smart_wb/LockScreenActivity.kt
+++ app/src/main/java/com/example/smart_wb/LockScreenActivity.kt
@@ -1,7 +1,10 @@
 package com.example.smart_wb
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 /**
  * 20/05/31 yama 잠금화면 액티비티
  * */
@@ -12,5 +15,12 @@ class LockScreenActivity : AppCompatActivity() {
 
         //초기 바텀네비게이션 세팅
         supportFragmentManager.beginTransaction().replace(R.id.frame, FragmentMainTimer()).commit()
+        Log.d("락스크린액티비티", "onCreate: 여기로들어와지나")
+
+    }
+
+    override fun onBackPressed() {
+        // 뒤로가기 버튼 클릭
+        Log.d(TAG, "onBackPressed: 뒤로가기 버튼 제어")
     }
 }
