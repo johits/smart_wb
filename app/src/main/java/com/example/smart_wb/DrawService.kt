837843fd5dbccf2713d1a2ee4fb74bea5a9ebd58
yamamamo <84963245+yamamamo@users.noreply.github.com>
2021-06-02 14:13:53 +0900

    타이머 작업중

--- app/src/main/java/com/example/smart_wb/DrawService.kt
+++ app/src/main/java/com/example/smart_wb/DrawService.kt
@@ -9,7 +9,10 @@ import android.util.Log
 import android.view.*
 import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.concurrent.timer
 
 
 /**2021-06-01
@@ -32,8 +35,9 @@ class DrawService : Service() {
     var mView: View? = null
 
     var handler: Handler? = null
-    var flag : Boolean= true
+    var thread : Thread?= null
 
+    var settingTime =0
     override fun onBind(p0: Intent?): IBinder {
 //        throw UnsupportedOperationException("Not yet")
         return mMessenger.binder
@@ -41,9 +45,9 @@ class DrawService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         callEvent()
-//        handler = Handler()
-//        var thread = StartTimer()
-//        handler?.post(thread)
+        handler = Handler()
+        thread = StartTimer()
+        handler?.post(thread as StartTimer)
         return Service.START_STICKY
     }
 
@@ -71,6 +75,7 @@ class DrawService : Service() {
         val bt = mView!!.findViewById<View>(R.id.btStop) as Button
         bt.setText("종료")
         bt.setOnClickListener {
+            settingTime=-1
             sendMsgToActivity(1234);
             Log.d(TAG, "종료버튼 클릭")
             val intent = Intent(applicationContext, MainActivity::class.java)
@@ -90,27 +95,70 @@ class DrawService : Service() {
                 mView = null
             }
             wm = null
-            flag=false
+
         }
     }
 
     //타이머쓰레드 이너클래스
     inner class StartTimer : Thread() {
+        @RequiresApi(Build.VERSION_CODES.N)
         override fun run() {
-            if(flag){
-               val time = SimpleDateFormat("HH:mm:ss").format(System.currentTimeMillis());
+            if(settingTime>=0){
+                    val watch = mView!!.findViewById<View>(R.id.tvWatch) as TextView
+
+                if(settingTime==0){
+
+                }else{
+                    watch.text = calTime(settingTime)
+                }
+                    handler?.postDelayed(this, 1000)
+                    settingTime--
+                    Log.d(TAG, "settingTime:" + settingTime)
+            }else{
                 val watch = mView!!.findViewById<View>(R.id.tvWatch) as TextView
-                watch.text = time
-                handler?.postDelayed(this, 1000)
+                watch.text="성공"
+                Log.d(TAG, "타이머종료")
             }
         }
+    }
+    //시간변환기
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun calTime(setTime: Int): String? {
+        var result: String?
+        val hour = Math.floorDiv(setTime, 3600)
+        val min = Math.floorMod(setTime, 3600) / 60
+        val sec = Math.floorMod(setTime, 3600) % 60
+
+        if (hour > 0) {
+            if (min < 10 && sec < 10) {
+                result = "${hour}:0${min}:0${sec}"
+            }else if(min<10&&sec>10){
+                result = "${hour}:0${min}:${sec}"
+            }else if(min>10&&sec<10){
+                result = "${hour}:${min}:0${sec}"
+            }else{
+                result = "${hour}:${min}:${sec}"
+            }
+        } else {
+//            Log.d("tag", "시간0")
+            if (min < 10 && sec < 10) {
+                result = "0${min}:0${sec}"
+            }else if(min<10&&sec>10){
+                result = "0${min}:${sec}"
+            }else if(min>10&&sec<10){
+                result = "${min}:0${sec}"
+            }else{
+                result = "${min}:${sec}"
+            }
+        }
+
+        return result
     }
 
 //     activity로부터 binding 된 Messenger
     private val mMessenger = Messenger(Handler { msg ->
         Log.d("tag", " message what : " + msg.what + " , msg.obj " + msg.obj)
-        val watch = mView!!.findViewById<View>(R.id.tvWatch) as TextView
-        watch.text = msg.obj.toString()
+        settingTime= msg.obj as Int
         when (msg.what) {
             MSG_REGISTER_CLIENT -> mClient = msg.replyTo // activity로부터 가져온
 
@@ -122,7 +170,7 @@ class DrawService : Service() {
         try {
             val bundle = Bundle()
             bundle.putInt("fromService", sendValue)
-            bundle.putString("test", "abcdefg")
+            bundle.putString("result", "finish")
             val msg: Message = Message.obtain(null, MSG_SEND_TO_ACTIVITY)
             msg.setData(bundle)
             mClient!!.send(msg) // msg 보내기
