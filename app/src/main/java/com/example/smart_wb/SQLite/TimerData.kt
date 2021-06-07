4c48cb49852696c6dd71551c776c868a0cf36957
yamamamo <84963245+yamamamo@users.noreply.github.com>
2021-06-07 09:58:21 +0900

    sqlite 작업 주석작성

--- app/src/main/java/com/example/smart_wb/SQLite/TimerData.kt
+++ app/src/main/java/com/example/smart_wb/SQLite/TimerData.kt
@@ -4,8 +4,8 @@ package com.example.smart_wb.SQLite
  * */
 data class TimerData(
     val id:Int, //pk
-    val date:String,
-    val time:String,
-    val settingTime:Int,
+    val date:String,//시작날짜
+    val time:String,//시작시간
+    val settingTime:Int, //설정시간
     val success:Int //sqlite boolean 사용x. 성공 1, 실패 0
 )
