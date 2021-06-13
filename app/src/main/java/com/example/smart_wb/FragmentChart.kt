fdb5ee8395d837e1199755d758b5dc9bd330f9ed
johits <74091087+johits@users.noreply.github.com>
2021-06-13 15:52:20 +0900

    차트 수정 중

--- app/src/main/java/com/example/smart_wb/FragmentChart.kt
+++ app/src/main/java/com/example/smart_wb/FragmentChart.kt
@@ -1,10 +1,18 @@
 package com.example.smart_wb
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.android.synthetic.main.fragment_chart.*
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +28,8 @@ class FragmentChart : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    var labelList = ArrayList<String>()
+    var valList = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +47,68 @@ class FragmentChart : Fragment() {
         return inflater.inflate(R.layout.fragment_chart, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+//        //현재 시간 가져오기.
+//        long now = System.currentTimeMillis();
+//        // Date 생성하기
+//        Date date = new Date(now);
+//
+//        //가져오고 싶은 형식으로 가져오기
+//        SimpleDateFormat sdf = new SimpleDateFormat("MM월");
+//        String getTime = sdf.format(date);
+
+        chart_week.setOnClickListener(View.OnClickListener {
+            chart_week.setTextColor(Color.parseColor("#2FA9FF"))
+            chart_month.setTextColor(Color.parseColor("#000000"))
+            chart_year.setTextColor(Color.parseColor("#000000"))
+        })
+
+        chart_month.setOnClickListener(View.OnClickListener {
+            chart_month.setTextColor(Color.parseColor("#2FA9FF"))
+            chart_week.setTextColor(Color.parseColor("#000000"))
+            chart_year.setTextColor(Color.parseColor("#000000"))
+        })
+
+        chart_year.setOnClickListener(View.OnClickListener {
+            chart_year.setTextColor(Color.parseColor("#2FA9FF"))
+            chart_month.setTextColor(Color.parseColor("#000000"))
+            chart_week.setTextColor(Color.parseColor("#000000"))
+        })
+
+
+
+        labelList.add("성공")
+        labelList.add("실패")
+        valList.add(10)
+        valList.add(90)
+        PieChartGraph(labelList,valList)
+    }
+    private fun PieChartGraph(labelList: ArrayList<String>, valList: ArrayList<Int>) {
+        // PieChart 메소드
+
+        // PieChart 메소드
+//        val pieChart = findViewById<View>(R.id.chart) as PieChart
+        val entries =
+            ArrayList<Entry>()
+        for (i : Int in valList.indices) {
+            entries.add(Entry(valList[i].toFloat(), i))
+        }
+
+        val depenses = PieDataSet(entries, "스마트 웰빙 평균 사용량 체크")
+        depenses.axisDependency = YAxis.AxisDependency.LEFT
+        val labels = ArrayList<String>()
+        for (i in labelList.indices) {
+            labels.add(labelList[i])
+        }
+        val data = PieData(labels, depenses) // 라이브러리 v3.x 사용하면 에러 발생함
+        depenses.setColors(ColorTemplate.LIBERTY_COLORS) //
+        chart.data = data
+        chart.animateXY(1000, 1000)
+        chart.invalidate()
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
