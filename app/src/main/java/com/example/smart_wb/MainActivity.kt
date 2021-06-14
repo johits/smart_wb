8dfa5329993071783671a66162af4182217eee6c
yamamamo <84963245+yamamamo@users.noreply.github.com>
2021-06-14 14:20:52 +0900

    백버튼 이전 프레그먼트 전환 끝

--- app/src/main/java/com/example/smart_wb/MainActivity.kt
+++ app/src/main/java/com/example/smart_wb/MainActivity.kt
@@ -19,18 +19,23 @@ private lateinit var binding: ActivityMainBinding
 //2020-05-29 joker 메인 클래스 (프래그먼트 메뉴에 대한 코드) .
 
 class MainActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         private val TAG = "MainActivity"
     }
-    private val adapter by lazy {AdapterMainFragment(supportFragmentManager, 4)}
-    private lateinit var stack : Stack<Int>
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var stack: Stack<Int>//프래그먼트트 전환는 순서대로 스택에 푸시한다.
+
+    private val adapter by lazy { AdapterMainFragment(supportFragmentManager, 4) }
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        configureBottomNavigation()
 
@@ -58,22 +63,24 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
         // 뷰페이저 어댑터 연결
-        xml_main_viewpaper.adapter = MainActivity@adapter
+        xml_main_viewpaper.adapter = MainActivity@ adapter
 
-        xml_main_viewpaper.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+        xml_main_viewpaper.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
             }
 
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
             }
 
             override fun onPageSelected(position: Int) {
-                stack.push(position)
-                for(data in stack){
-                    Log.d(TAG, "데이터 입력 받음 stack:$data")
-                }
+
+                stack.push(position)//프래그먼트 포지션값 스택에 푸쉬
+
                 xml_main_tablayout.getTabAt(0)?.setIcon(R.drawable.mtimer1)
                 xml_main_tablayout.getTabAt(1)?.setIcon(R.drawable.mcalendar1)
                 xml_main_tablayout.getTabAt(2)?.setIcon(R.drawable.mchart1)
@@ -81,15 +88,14 @@ class MainActivity : AppCompatActivity() {
 //                xml_main_tablayout.getTabAt(4)?.setIcon(R.drawable.msetting1)
 
 
-                when(position) {
+                when (position) {
 
-                    0   ->    xml_main_tablayout.getTabAt(0)?.setIcon(R.drawable.mtimer2)
-                    1   ->   xml_main_tablayout.getTabAt(1)?.setIcon(R.drawable.mcalendar2)
-                    2   ->    xml_main_tablayout.getTabAt(2)?.setIcon(R.drawable.mchart2)
-                    3   ->    xml_main_tablayout.getTabAt(3)?.setIcon(R.drawable.mitem2)
+                    0 -> xml_main_tablayout.getTabAt(0)?.setIcon(R.drawable.mtimer2)
+                    1 -> xml_main_tablayout.getTabAt(1)?.setIcon(R.drawable.mcalendar2)
+                    2 -> xml_main_tablayout.getTabAt(2)?.setIcon(R.drawable.mchart2)
+                    3 -> xml_main_tablayout.getTabAt(3)?.setIcon(R.drawable.mitem2)
 //                    4   ->    xml_main_tablayout.getTabAt(4)?.setIcon(R.drawable.msetting2)
                 }
-
 
 
             }
@@ -108,21 +114,18 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //프래그먼트 화면 전환할 때 마다 스택에 포지션 값 푸쉬
+    //백버튼 누르면 스택 팝 한다.
+    //스택.peek() 으로 최상단 값 가져온다.
+    //가져온 값으로 화면 전환.
     override fun onBackPressed() {
-        //스택에 값이 없으면
-        if (stack.empty()) {
+        stack.pop()
+        if (stack.empty()) {   //스택에 값이 없으면 백버튼 정상 동작
             super.onBackPressed()
-        } else {
+        } else { //스택에 값이 있으면 이전 프래그먼트로 전환
+            Log.d(TAG, "스택 상단의 값 ${stack.peek()}")
+            xml_main_viewpaper.setCurrentItem(stack.peek())//프래그먼트 전환
             stack.pop()
-            if(stack.empty()){
-                super.onBackPressed()
-            }else{
-                Log.d(TAG, "스택 상단의 값 ${stack.peek()}")
-                for (data in stack) {
-                    Log.d(TAG, "팝하고 남은 데이터 stack:$data")
-                }
-
-            }
         }
     }
 }
